78c0a53ee7d958250aa92b00aa8d40f683ce9a7f
Karan Raina <karanraina1996@gmail.com>
2021-09-08 10:56:36 +0530

    added basic search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GithubService } from './services/github.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'assignment';
+  username: string;
+
+  repositories: any[] = [];
+
+  errorMessage: string = '';
+
+  fetchingData: boolean = false;
+
+  constructor(
+    private githubApi: GithubService
+  ) { }
+
+  handleClick() {
+    if (this.fetchingData) {
+      return;
+    }
+    this.fetchRepositories()
+  }
+
+  fetchRepositories() {
+    this.fetchingData = true;
+    this.githubApi.getRepoData(this.username).subscribe(
+      (response) => {
+        this.fetchingData = false;
+        this.repositories = response;
+      },
+      (error) => {
+        this.fetchingData = false;
+        this.errorMessage = error.message;
+      }
+    )
+  }
+
 }
